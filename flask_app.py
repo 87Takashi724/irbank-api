ecfacfa429604b739257f2079edba22a058a218a
亀山貴司 <kameyamatakashi@kameyamatakashinoMacBook-Air.local>
2025-08-01 20:58:42 +0900

    Add index route for top page

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,8 @@
-from flask import Flask, request, jsonify
-import requests
-import pandas as pd
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/irbank/financial', methods=['GET'])
-def get_financials():
-    code = request.args.get('code')
-    url = "https://irbank.net/download/fy-profit-and-loss.json"
-    res = requests.get(url)
-    data = pd.DataFrame(res.json())
-    df = data[data["証券コード"] == code]
-    return jsonify(df.to_dict(orient="records"))
+@app.route("/")
+def index():
+    return "IR Bank API is up and running!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
